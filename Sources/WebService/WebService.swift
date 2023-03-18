f9d7a6656c0fb356505b547df30eabcd16024ae2
Linkon Sid <linkon@sheba.xyz>
2023-03-18 13:47:23 +0600

    Add WebService for requesting remote data using async/await and Swift 5.7

--- Sources/WebService/WebService.swift
+++ Sources/WebService/WebService.swift
@@ -1,6 +1,29 @@
-public struct WebService {
-    public private(set) var text = "Hello, World!"
+//
+//  APIClient.swift
+//  GithubUserSearch
+//
+//  Created by Linkon Sid on 13/3/23.
+//
 
-    public init() {
+import Foundation
+
+// Generic Client that can be utilized by all the classes/structs that need to fetch data from remote location
+public protocol WebServiceProtocol{
+    func getData(for request: URLRequest) async throws -> (Data, URLResponse)
+}
+
+// default implementation
+extension WebServiceProtocol{
+    public func getData(for request: URLRequest) async throws -> (Data, URLResponse){
+        do {
+            let result = try await URLSession.shared.data(for: request)
+            return result
+        } catch{
+            throw error
+        }
     }
+}
+
+public struct WebService:WebServiceProtocol{
+
 }
--- Tests/WebServiceTests/WebServiceTests.swift
+++ Tests/WebServiceTests/WebServiceTests.swift
@@ -6,6 +6,6 @@ final class WebServiceTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(WebService().text, "Hello, World!")
+//        XCTAssertEqual(WebService().text, "Hello, World!")
     }
 }
