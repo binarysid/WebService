afc7fb89007a631fcf7595a56920cde367066ce5
Linkon Sid <linkon@sheba.xyz>
2023-03-30 19:46:13 +0600

    Json decoder added(protocol and implementation)

--- Sources/WebService/WebService.swift
+++ Sources/WebService/WebService.swift
@@ -8,22 +8,34 @@
 import Foundation
 
 // Generic Client that can be utilized by all the classes/structs that need to fetch data from remote location
-public protocol WebServiceProtocol{
+public protocol WebServiceProtocol {
     func getData(for request: URLRequest) async throws -> (Data, URLResponse)
+    func getDecodedModel<T: Decodable>(from data: Data, to type: T.Type) throws -> T
 }
 
 // default implementation
-extension WebServiceProtocol{
-    public func getData(for request: URLRequest) async throws -> (Data, URLResponse){
+extension WebServiceProtocol {
+
+    public func getData(for request: URLRequest) async throws -> (Data, URLResponse) {
         do {
             let result = try await URLSession.shared.data(for: request)
             return result
-        } catch{
+        } catch {
             throw error
         }
     }
+
+    public func getDecodedModel<T: Decodable>(from data: Data, to type: T.Type) throws -> T {
+        do {
+            let userData = try JSONDecoder().decode(T.self, from: data)
+            return userData
+        } catch {
+            throw error
+        }
+    }
+
 }
 
-public struct WebService:WebServiceProtocol{
-    public init(){}
+public struct WebService: WebServiceProtocol {
+    public init() {}
 }
